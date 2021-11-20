9583b48353e7f589ebc46dd45c03e27dfd03e1b4
lmnjcx <81728420+ssssshel@users.noreply.github.com>
2021-11-20 03:40:25 -0500

    Final?

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ const cboLanguage = document.getElementById('cboLanguage');
 
 cboLanguage.addEventListener('change', (e) => {
     let res = e.target.value;
-    if(res = 'en'){
-        location.href = 'index.html'
-    }else if(res = 'es'){
-        location.href = 'es/index.html';
+    if(res == 'en'){
+        window.location.href = '../index.html'
+    }else if(res == 'es'){
+        window.location.href = 'es/index.html';
     }else{
         console.log('error');
     }
